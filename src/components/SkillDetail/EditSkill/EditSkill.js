fe9b71d6531f2fce87464da3d371d41bdd3c3e77
jaymark <jaykmark@gmail.com>
2020-06-12 21:22:57 -0700

    Make button focus pseudo class consistent with hover and active.

--- src/components/SkillDetail/EditSkill/EditSkill.js
+++ src/components/SkillDetail/EditSkill/EditSkill.js
@@ -33,7 +33,7 @@ class EditSkill extends React.Component {
       <>
         {/* EditSkill Button */}
         <button className="btn btn-success btn-sm float-right btn-editSkill" type="button"
-        id="register" data-toggle="modal" data-target="#editSkill">EDIT SKILL</button>
+          id="register" data-toggle="modal" data-target="#editSkill">EDIT SKILL</button>
 
         {/* EditSkill Modal */}
         <div className="container">
@@ -41,77 +41,77 @@ class EditSkill extends React.Component {
             <div className="modal-dialog" role="document">
               <div className="modal-content">
                 <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Edit Skill</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              
-              <div className="modal-body">
-                <form>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      onChange={this.handleChange}
-                      className="form-control form-control-lg"
-                      type="text"
-                      id="name"
-                      name="name"
-                      value={this.state.name}
-                      required
-                      minLength="1"
-                      maxLength="200"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="totalMinutes">Total Minutes</label>
-                    <input
-                      onChange={this.handleChange}
-                      className="form-control form-control-lg"
-                      type="number"
-                      id="totalMinutes"
-                      name="totalMinutes"
-                      value={this.state.totalMinutes}
-                      required
-                      minLength="1"
-                      maxLength="10"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="description">Description</label>
-                    <textarea
-                      onChange={this.handleChange}
-                      className="form-control form-control-md"
-                      type="text"
-                      id="description"
-                      name="description"
-                      value={this.state.description}
-                      minLength="1"
-                      maxLength="200"
-                    ></textarea>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="notes">Notes</label>
-                    <textarea
-                      onChange={this.handleChange}
-                      className="form-control form-control-sm"
-                      type="text"
-                      id="notes"
-                      name="notes"
-                      value={this.state.notes}
-                      minLength="1"
-                      maxLength="500"
-                    ></textarea>
-                  </div>
-                  <button data-dismiss="modal" onClick={(event) => this.props.editSkill(event, this.state)} className="btn btn-primary float-right" type="submit">
-                    Edit Skill
+                  <h5 className="modal-title" id="exampleModalLabel">Edit Skill</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                   </button>
-                </form>
+                </div>
+
+                <div className="modal-body">
+                  <form>
+                    <div className="form-group">
+                      <label htmlFor="name">Name</label>
+                      <input
+                        onChange={this.handleChange}
+                        className="form-control form-control-lg"
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={this.state.name}
+                        required
+                        minLength="1"
+                        maxLength="200"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="totalMinutes">Total Minutes</label>
+                      <input
+                        onChange={this.handleChange}
+                        className="form-control form-control-lg"
+                        type="number"
+                        id="totalMinutes"
+                        name="totalMinutes"
+                        value={this.state.totalMinutes}
+                        required
+                        minLength="1"
+                        maxLength="10"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="description">Description</label>
+                      <textarea
+                        onChange={this.handleChange}
+                        className="form-control form-control-md"
+                        type="text"
+                        id="description"
+                        name="description"
+                        value={this.state.description}
+                        minLength="1"
+                        maxLength="200"
+                      ></textarea>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="notes">Notes</label>
+                      <textarea
+                        onChange={this.handleChange}
+                        className="form-control form-control-sm"
+                        type="text"
+                        id="notes"
+                        name="notes"
+                        value={this.state.notes}
+                        minLength="1"
+                        maxLength="500"
+                      ></textarea>
+                    </div>
+                    <button data-dismiss="modal" onClick={(event) => this.props.editSkill(event, this.state)} className="btn btn-primary float-right" type="submit">
+                      Save
+                  </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
       </>
     )
   }
